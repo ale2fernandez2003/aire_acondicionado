c2f02768e318b926f44b717d44109f22605670d5
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2022-01-19 13:19:35 +0100

    0497.02 subi y baje la temperatura

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -8,26 +8,28 @@
 public class AireAcondicionado
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private double temperatura;
+    private double subirTemperatura;
+    private double bajarTemperatura;
 
     /**
      * Constructor for objects of class AireAcondicionado
      */
-    public AireAcondicionado()
-    {
-        // initialise instance variables
-        x = 0;
+    public AireAcondicionado (){
+        temperatura = 15.0;
+        subirTemperatura = 5.0;
+        bajarTemperatura = 5.0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void subirTemperatura (){
+        temperatura = temperatura + subirTemperatura;
+    }
+    
+    public void bajarTemperatura (){
+        temperatura = temperatura - bajarTemperatura;
+    }
+    
+    public double getTemperatura (){
+        return temperatura;
     }
 }
